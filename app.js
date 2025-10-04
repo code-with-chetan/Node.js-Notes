ecd9368153832904bc4454619e45a0335a9d41b8
chetan rana <ranachetan455@gmail.com>
2025-10-04 14:19:18 +0545

    Day1:learn module,import,exports in Node.js

---
+++ app.js
@@ -0,0 +1,13 @@
+// const add=require("./math");
+// console.log(add(20,20));
+//commonjs
+
+
+const {add,sub,mul,div}=require("./math");
+console.log(add(5,5));
+console.log(sub(5,5));
+console.log(mul(5,5));
+console.log(div(5,5));
+
+
+
