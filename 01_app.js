c07bdf92043c43eb1940f062af89424b7e2fb4f2
chetan rana <ranachetan455@gmail.com>
2025-10-04 21:06:52 +0545

    Day1:learn about import,exports,os module and path module etc

--- 01_app.js
+++ 01_app.js
@@ -3,7 +3,7 @@
 //commonjs
 
 //Named and Aggregate Exports in Node.js
-const {add,sub,mul,div}=require("./math");
+const {add,sub,mul,div}=require("./01_math");
 console.log(add(5,5));
 console.log(sub(5,5));
 console.log(mul(5,5));
