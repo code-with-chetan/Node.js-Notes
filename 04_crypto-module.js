a138dc85d3307b9f6d5ebc00755249c9ca67b845
chetan rana <ranachetan455@gmail.com>
2025-10-05 12:41:00 +0545

    Day2:learn about crypto module

---
+++ 04_crypto-module.js
@@ -0,0 +1,10 @@
+const crypto=require("crypto");
+const randomValue=crypto.randomBytes(8).toString("hex");
+console.log(randomValue);
+
+const hashValue=crypto.createHash("sha256").update("rana coder").digest("hex");
+console.log(hashValue);
+
+const hashValue1=crypto.createHash("sha256").update("rana").digest("hex");
+console.log(hashValue1);
+
