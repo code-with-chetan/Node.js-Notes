8b21911c9436e4cbafaf248fef513c3597f37356
chetan rana <ranachetan455@gmail.com>
2025-10-11 20:02:40 +0545

    serving static files in express.js

---
+++ 11_express/main.js
@@ -0,0 +1,20 @@
+import express from "express";
+import { PORT } from "./env.js";
+import path from "path";
+
+const app = express();
+//prefer absolute path.
+const staticPath = path.join(import.meta.dirname, "public");
+app.use(express.static(staticPath));
+
+app.get("/", (req, res) => {
+  const _fileName = new URL(import.meta.url).pathname;
+  //   console.log(_fileName);
+  //   console.log(import.meta.dirname);
+  //   const homePagePath = path.join(import.meta.dirname, "public", "index.html");
+  //   res.sendFile(homePagePath);
+});
+
+app.listen(PORT, () => {
+  console.log(`server running at http:localhost//${PORT}`);
+});
