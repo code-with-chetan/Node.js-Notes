d1b5854f17c4c29806c498a5fcb8d8a8c774c673
chetan rana <ranachetan455@gmail.com>
2025-10-11 11:40:54 +0545

    Starting express.js

---
+++ 11_express/app.js
@@ -0,0 +1,36 @@
+import express from "express";
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.get("/", (req, res) => res.send("<h1>Hello Home page.</h1>"));
+app.get("/about", (req, res) => res.send("<h1>About page.</h1>"));
+app.get("/contact", (req, res) => {
+  return res.send(`
+        
+   <div class="container">
+       <h1>URL Shortener</h1>
+         <form id="shorten-form">
+         <div>
+            <label for="url">Enter URL:</label>
+            <input type="url" name="url" id="url" required />
+        </div>
+
+        <div>
+           <label for="shortCode">Enter shortCode:</label>
+           <input type="text" name="shortCode" id="shortCode" required />
+        </div>
+          <button type="submit">Shorten</button>
+      </form>
+
+        <h2>Shortened URLS:</h2>
+        <ul id="shortened-urls"></ul>
+    </div>
+
+  `);
+});
+
+app.listen(PORT, () => {
+  console.log(`server is running at http://localhost:${PORT}`);
+});
