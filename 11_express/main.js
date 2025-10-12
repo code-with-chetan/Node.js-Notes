a63a2490d21d5712b7e7261c981708529d581068
chetan rana <ranachetan455@gmail.com>
2025-10-12 21:21:39 +0545

    form submission in express.js post method

--- 11_express/main.js
+++ 11_express/main.js
@@ -4,6 +4,14 @@ import path from "path";
 
 const app = express();
 
+//prefer absolute path.
+const staticPath = path.join(import.meta.dirname, "public");
+app.use(express.static(staticPath));
+app.use(express.urlencoded({ extended: true }));
+app.use((req, res) => {
+  res.status(404).send("page not found.");
+});
+
 //In newer versions of Node.js (14.8+) ,you can use top-level await without needing to wrap it in an
 //async function.
 const response = await fetch(
@@ -12,21 +20,30 @@ const response = await fetch(
 const data = await response.json();
 console.log(data);
 
+//form submission
+app.post("/contact", (req, res) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
 //finding current directory and filepath.
 // console.log(import.meta.dirname);
 // console.log(import.meta.filename);
 
-//prefer absolute path.
-const staticPath = path.join(import.meta.dirname, "public");
-app.use(express.static(staticPath));
+//query parameters.
+// app.get("/product", (req, res) => {
+//   console.log(req.query);
+//   res.send(`<h1>Hello product page display:${req.query.search}</h1>`);
+// });
 
-app.get("/public/:username/article/:slug", (req, res) => {
-  console.log(req.params);
-  const formattedSlog = req.params.slug.replace(/-/g, " ");
-  res.send(
-    `<h1>Hello my name is ${req.params.username} by ${formattedSlog}</h1>`
-  );
-});
+//route parameters.
+// app.get("/public/:username/article/:slug", (req, res) => {
+//   console.log(req.params);
+//   const formattedSlog = req.params.slug.replace(/-/g, " ");
+//   res.send(
+//     `<h1>Hello my name is ${req.params.username} by ${formattedSlog}</h1>`
+//   );
+// });
 
 // app.get("/", (req, res) => {
 //   const _fileName = new URL(import.meta.url).pathname;
