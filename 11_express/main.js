78df73fb8560e6e924558c14d9ee253c74abf2cb
chetan rana <ranachetan455@gmail.com>
2025-10-12 17:54:23 +0545

    learn about top level await

--- 11_express/main.js
+++ 11_express/main.js
@@ -3,6 +3,19 @@ import { PORT } from "./env.js";
 import path from "path";
 
 const app = express();
+
+//In newer versions of Node.js (14.8+) ,you can use top-level await without needing to wrap it in an
+//async function.
+const response = await fetch(
+  "https://official-joke-api.appspot.com/random_joke"
+);
+const data = await response.json();
+console.log(data);
+
+//finding current directory and filepath.
+console.log(import.meta.dirname);
+console.log(import.meta.filename);
+
 //prefer absolute path.
 const staticPath = path.join(import.meta.dirname, "public");
 app.use(express.static(staticPath));
