2a12c31fb307f7e7b80a8a665b73d6bdd0c0c7f2
chetan rana <ranachetan455@gmail.com>
2025-10-17 17:14:05 +0545

    learn CURD Operations in mongodb

--- mongodb/mongo-driver.js
+++ mongodb/mongo-driver.js
@@ -6,4 +6,29 @@ await client.connect();
 
 const db = client.db("mongodb_nodejs");
 const userCollections = db.collection("users");
-userCollections.insertOne({ name: "Chetan Rana", age: 21 });
+
+// await userCollections.insertOne({ name: "Chetan Rana", age: 21 });
+
+// await userCollections.insertMany([
+//   { name: "pawan k.c", age: 23 },
+//   { name: "Ram Shah", age: 32 },
+//   { name: "Chetan Rana", age: 22 },
+// ]);
+
+//Read
+// const userCursor = await userCollections.find().toArray();
+// console.log(userCursor);
+
+// const user = await userCollections.findOne({ name: "pawan k.c" });
+// console.log(user);
+// console.log(user._id.toHexString());
+
+//update
+// await userCollections.updateOne({ name: "Ram Shah" }, { $set: { age: 30 } });
+// await userCollections.updateMany(
+//   { name: "Chetan Rana" },
+//   { $set: { age: 20 } }
+// );
+
+//delete
+await userCollections.deleteMany({ name: "Chetan Rana" });
