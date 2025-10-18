62c96835ad051d39fa219f08101ae20f1851a839
chetan rana <ranachetan455@gmail.com>
2025-10-18 13:06:59 +0545

    middleware in mongoose

---
+++ mongoose/mongoose-middleware.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+await mongoose.connect("mongodb://localhost:27017/mongoose-middleware");
+mongoose.set("debug", true);
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  age: Number,
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+});
+
+userSchema.pre(["updateOne", "findOneAndUpdate"], function (next) {
+  console.log("🔥 Middleware triggered!");
+  this.set({ updatedAt: Date.now() });
+  next();
+});
+
+const User = mongoose.model("User", userSchema);
+
+// check if doc exists
+const found = await User.findOne({ name: "Chetan Rana" });
+console.log("📄 Found user:", found);
+
+const result = await User.updateOne(
+  { name: "Chetan Rana" },
+  { $set: { age: 25 } }
+);
+
+console.log("✅ Update result:", result);
+
+await mongoose.connection.close();
