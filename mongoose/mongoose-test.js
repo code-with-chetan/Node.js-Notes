63b74265f9ce98c219077dad6ca1cc28e11f9798
chetan rana <ranachetan455@gmail.com>
2025-10-18 12:00:14 +0545

    CURD Operations in Mongoose

--- mongoose/mongoose-test.js
+++ mongoose/mongoose-test.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 //step:1 connect to the mongodb server.
 try {
   await mongoose.connect("mongodb://localhost:27017/mongoose-database");
-  mongoose.set("debug", true);
+  // mongoose.set("debug", true);
 } catch (error) {
   console.error(error);
   process.exit();
@@ -18,7 +18,32 @@ const userSchema = mongoose.Schema({
 });
 
 //step3:creating a model
-const user = mongoose.model("user", userSchema);
+const User = mongoose.model("user", userSchema);
 
-await user.create({ name: "Chetan Rana", email: "chetan@gmail.com", age: 22 });
+// await user.create({ name: "Chetan Rana", email: "chetan@gmail.com", age: 22 });
+const userData = [
+  { name: "Chetan rana", email: "chetan450@gmail.com", age: 20 },
+  { name: "pawan k.c", email: "pawan@gmail.com", age: 21 },
+  { name: "Birendra Shahi", email: "birendra@gmail.com", age: 22 },
+];
+
+//?Insert Operation.
+// await User.insertMany(userData);
+
+//?Read Operation.
+// const users = await User.find();
+// const users = await User.find({ age: { $gt: 20 } });
+// console.log(users);
+
+//?Update Operation.
+// const userUpdate = await User.updateOne(
+//   { email: "birendra@gmail.com" },
+//   { $set: { age: 30 } }
+// );
+// console.log(userUpdate);
+
+//?Delete Operation.
+const deleteUser = await User.deleteMany({ email: "birendra@gmail.com" });
+console.log(deleteUser);
+
 await mongoose.connection.close();
